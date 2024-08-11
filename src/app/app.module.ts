b6209ac8f3612e9f2906f68958a6edce83bdb586
Kal <info@kalacademy.org>
2024-08-11 11:09:51 -0700

    Created current weather component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CurrentWeatherComponent } from "./current-weather/current-weather.component";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    CurrentWeatherComponent
+],
   providers: [],
   bootstrap: [AppComponent]
 })
